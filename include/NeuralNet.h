b32766d4af0707e059474d2bc3d4ef969f4a66cd
Ferriol Pey Comas <ferriol73pey@gmail.com>
2022-08-23 04:01:02 +0200

    First draft of the NeuralNet Class

--- include/NeuralNet.h
+++ include/NeuralNet.h
@@ -1,1 +1,14 @@
 #pragma once
+
+#include "Matrix.h"
+
+class NeuralNet
+{
+    std::vector<int> _net_topology;
+    std::vector<Matrix> _net_weights;
+    std::vector<Matrix> _net_values; //?
+    std::vector<Matrix> _net_bias; 
+public:
+    void save(std::string path_to_save);
+    void load(std::string path_to_load);
+}
